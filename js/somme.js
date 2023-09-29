2c76d5386866a485dc53c47b184542a86fd59251
bambamasso <bambamasso51@gmail.com>
2023-09-29 10:03:41 +0100

    dernière modification

--- js/somme.js
+++ js/somme.js
@@ -1,10 +1,10 @@
 
-let fixe =localStorage.getItem("BUDGET_FIXE")?JSON.parse(localStorage.getItem("BUDGET_FIXE")):[];
+let somme =localStorage.getItem("BUDGET_FIXE")?JSON.parse(localStorage.getItem("BUDGET_FIXE")):[];
 let variable =localStorage.getItem("BUDGET_VARIABLE")?JSON.parse(localStorage.getItem("BUDGET_VARIABLE")):[];
 let occasion =localStorage.getItem("BUDGET_OCCASIONNELLE")?JSON.parse(localStorage.getItem("BUDGET_OCCASIONNELLE")):[];
 let somation=0;
-const s1=fixe.length?fixe.reduce((a,b)=> Number(a.montant)+Number(b.montant)): 0;
+const s1=somme.length?somme.reduce((a,b)=> Number(a.montant)+Number(b.montant)): 0;
 const s2=variable.length? variable.length>1?variable.reduce((a,b)=>Number(a.montant)+Number(b.montant)):variable[0].montant: 0;
 const s3=occasion.length?occasion.reduce((a,b)=> Number(a.montant)+Number(b.montant)): 0;
-console.log(fixe.reduce((a,b)=> Number(a.montant)+Number(b.montant)),s2,s3);
-document.querySelector(".prix").textContent=(s1+s2+s3)+ " FCFA";
+// console.log(somme.reduce((a,b)=> Number(a.montant)+Number(b.montant)),s2,s3);
+document.querySelector(".prix").innerHTML=somation+(s1+s2+s3)+ " FCFA";
